ecd095e08015fafd9162df96bbecf0ce6a623f43
maharadrzi <kubel_x@op.pl>
2015-01-18 20:05:18 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "source.h"
+
+using namespace std;
+
+int main()
+{
+	
+	char pole_gracza[10][10];
+	menu();
+
+	
+
+	//zer(pole_gracza);
+	//rys(pole_gracza);
+	
+
+
+
+	system("PAUSE");
+}
